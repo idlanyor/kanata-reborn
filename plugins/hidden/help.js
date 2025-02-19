ac05a355aa2449dbf2a7a48bb301b91286776237
prasbhara0604 <prasbhara0604@gmail.com>
2025-02-19 06:14:29 +0000

    y

--- plugins/hidden/help.js
+++ plugins/hidden/help.js
@@ -26,7 +26,7 @@ export default async ({ sock, id, m, noTel, sender }) => {
         if (plugin.toUpperCase() !== 'HIDDEN') {
             sections.push({
                 header: '╭─「 KANATA BOT MENU 」',
-                highlight_label: '2.0',
+                highlight_label: '3.0',
                 title: `${emojis[plugin.toUpperCase()] || '📌'} ${plugin.toUpperCase()} MENU`,
                 rows: plugins[plugin].map((command) => {
                     const cmdName = Array.isArray(command.handler)
@@ -69,7 +69,7 @@ export default async ({ sock, id, m, noTel, sender }) => {
 ├ *Date:* ${time.toLocaleDateString()}
 │
 ├ *Bot Info:*
-├ Version: 2.0
+├ Version: 3.0
 ├ Library: @seeavey/baileys
 ├ Platform: NodeJS
 │
