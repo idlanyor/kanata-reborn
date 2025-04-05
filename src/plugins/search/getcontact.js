ca93b8cf65b67f5bce1288238ce066a04ad92fcb
idlanyor <prasbhara0604@gmail.com>
2025-04-05 21:39:43 +0700

    enable gtc

--- src/plugins/search/getcontact.js
+++ src/plugins/search/getcontact.js
@@ -4,7 +4,17 @@ export const handler = 'getcontact'
 export const description = 'Retrieve Tags & User Information From Getcontact'
 export default async ({ sock, m, id, psn, sender, noTel, caption, attf }) => {
     if (!psn) return await sock.sendMessage(id, { text: 'Silahkan Masukkan nomor telepon dengan format `.getcontact 62xxxx`' })
-    const base = `https://fastrestapis.fasturl.cloud/tool/getcontact?number=${psn}`
+    
+    // Format nomor telepon
+    let formattedNumber = psn
+    if (psn.startsWith('@')) {
+        formattedNumber = psn.substring(1)
+    }
+    if (psn.startsWith('08')) {
+        formattedNumber = '62' + psn.substring(1)
+    }
+    
+    const base = `https://fastrestapis.fasturl.cloud/tool/getcontact?number=${formattedNumber}`
 
     const { data } = await axios.get(base)
     const text = `
